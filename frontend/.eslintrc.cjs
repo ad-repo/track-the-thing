b3d916124b486c1fc638c907c772f0528b052670
billytimmy666 <100006906+billytimmy666@users.noreply.github.com>
2025-11-22 18:05:13 -0500

    chore(frontend): relax eslint rules

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -10,12 +10,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react-refresh/only-export-components': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 }
 
